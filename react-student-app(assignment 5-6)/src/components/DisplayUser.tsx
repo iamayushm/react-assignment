fdebdceaee2ee010cadef0441eb4d9ea65ff0147
ayushmaheshwari <ayush@devtron.ai>
2023-02-01 12:42:52 +0530

    adding css

--- react-student-app(assignment 5-6)/src/components/DisplayUser.tsx
+++ react-student-app(assignment 5-6)/src/components/DisplayUser.tsx
@@ -1,32 +1,49 @@
 import { useUserContext } from "../context/userContext";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 
 export function DisplayUser() {
   let navigate = useNavigate();
   let { userData, removeUser } = useUserContext();
   return (
-    <ul className="list-group">
-      {userData.length == 0
-        ? "No users found"
-        : userData.map(({ id, name, email, gender }) => {
-            return (
-              <>
-                <li className="list-group-item">
-                  name: {name} email: {email} gender: {gender}
-                </li>
-                <button
-                    onClick={() => removeUser(id)}
-                  >
-                    Delete
-                  </button>
-                <button
-                  onClick={() => navigate("/user/edit/" + id)}
-                >
-                  edit
-                </button>
-              </>
-            );
-          })}
-    </ul>
+    <>
+      {userData.length == 0 ? (
+        "No users found"
+      ) : (
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <th scope="row"></th>
+              {userData.map(({ id, name, email, gender }) => {
+                return (
+                  <tr>
+                    <td>{id}</td>
+                    <td>{name}</td>
+                    <td>{email}</td>
+                    <td>{gender}</td>
+                    <td>
+                      <button onClick={() => navigate("/user/edit/" + id)}>
+                        edit
+                      </button>
+                    </td>
+                    <td>
+                      <button onClick={() => removeUser(id)}>Delete</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
+    </>
   );
 }
